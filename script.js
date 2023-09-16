3fddbc959ce7608cef0f0a50487e85eee9a9fb1a
Sanskar Gupta <130235683+Code-4-You@users.noreply.github.com>
2023-09-16 20:15:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+function initilize(){
+ var html = document.getElementById("html");
+ var css = document.getElementById("css");
+ var javascript = document.getElementById("javascript");
+ var code = document.getElementById("code").contentWindow.document;
+ 
+ document.body.onkeyup = function(){
+ 
+    code.open();
+    
+    code.writeln(
+
+        html.value + 
+
+        "<style>" + css.value + "</style>" +
+
+        "<script>" + javascript.value + "</script>"
+        
+
+    );
+
+    code.close();
+ }
+
+}
+initilize();
